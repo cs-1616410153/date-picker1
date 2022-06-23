f91a044dfa8cbf0f899a59b9fe9a037fd640ecf2
nvermanidhi644@gmail.com <nvermanidhi644@gmail.com>
2022-06-23 11:57:09 +0530

    used moment fro date-range

--- src/App.js
+++ src/App.js
@@ -1,140 +1,157 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
 
-export default function Calendar(props) {
-  const [selectedMonth, setSelectedMonth] = useState(new Date());
+// export default function Calendar(props) {
+//   const [selectedMonth, setSelectedMonth] = useState(new Date());
 
-  let day = selectedMonth.getDate();
-  let month = selectedMonth.getMonth();
-  let year = selectedMonth.getFullYear();
+//   let day = selectedMonth.getDate();
+//   let month = selectedMonth.getMonth();
+//   let year = selectedMonth.getFullYear();
 
-  const changeMonth = (type) => {
-    let m = new Date();
-    if (type === "prev")
-      m = new Date(
-        selectedMonth.getFullYear(),
-        selectedMonth.getMonth() - 1,
-        1
-      );
+//   const changeMonth = (type) => {
+//     let m = new Date();
+//     if (type === "prev")
+//       m = new Date(
+//         selectedMonth.getFullYear(),
+//         selectedMonth.getMonth() - 1,
+//         1
+//       );
 
-    if (type === "next")
-      m = new Date(
-        selectedMonth.getFullYear(),
-        selectedMonth.getMonth() + 1,
-        1
-      );
-    setSelectedMonth(m);
-  };
+//     if (type === "next")
+//       m = new Date(
+//         selectedMonth.getFullYear(),
+//         selectedMonth.getMonth() + 1,
+//         1
+//       );
+//     setSelectedMonth(m);
+//   };
 
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
+//   let months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+//   ];
+//   const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
 
-  const this_month = new Date(year, month, 1);
-  const next_month = new Date(year, month + 1, 1);
+//   const this_month = new Date(year, month, 1);
+//   const next_month = new Date(year, month + 1, 1);
 
-  const first_week_day = this_month.getDay() + 1;
-  const days_in_this_month = Math.round(
-    (next_month.getTime() - this_month.getTime()) / (1000 * 60 * 60 * 24)
-  );
+//   const first_week_day = this_month.getDay() + 1;
+//   const days_in_this_month = Math.round(
+//     (next_month.getTime() - this_month.getTime()) / (1000 * 60 * 60 * 24)
+//   );
 
-  const updateSelectedDate = (daySl) => {
-    let newDl = `${daySl}-${selectedMonth.getMonth()}-${selectedMonth.getFullYear()}`;
-    console.log(newDl);
-  };
+//   const updateSelectedDate = (daySl) => {
+//     let newDl = `${daySl}-${selectedMonth.getMonth()}-${selectedMonth.getFullYear()}`;
+//     console.log(newDl);
+//   };
 
-  const getDays = () => {
-    let daysTemplate = [];
+//   const getDays = () => {
+//     let daysTemplate = [];
 
-    let emptyRows = [];
-    for (let week_day = 1; week_day < first_week_day; week_day++) {
-      emptyRows.push(
-        <td style={{ backgroundColor: "9999cc", color: "000000" }}> </td>
-      );
-    }
+//     let emptyRows = [];
+//     for (let week_day = 1; week_day < first_week_day; week_day++) {
+//       emptyRows.push(
+//         <td style={{ backgroundColor: "9999cc", color: "000000" }}> </td>
+//       );
+//     }
 
-    let week_day = first_week_day;
+//     let week_day = first_week_day;
 
-    let rowData = [];
-    for (
-      let day_counter = 1;
-      day_counter <= days_in_this_month;
-      day_counter++
-    ) {
-      week_day %= 7;
-      if (emptyRows.length > 0) {
-        rowData.push(emptyRows);
-        emptyRows = [];
-      }
+//     let rowData = [];
+//     for (
+//       let day_counter = 1;
+//       day_counter <= days_in_this_month;
+//       day_counter++
+//     ) {
+//       week_day %= 7;
+//       if (emptyRows.length > 0) {
+//         rowData.push(emptyRows);
+//         emptyRows = [];
+//       }
 
-      rowData.push(
-        <td
-          onClick={() => updateSelectedDate(day_counter)}
-          style={{
-            backgroundColor: "9999cc",
-            color: "000000",
-            textAlign: "center",
-          }}
-        >
-          {day_counter}
-        </td>
-      );
-      if (week_day === 0 || day_counter === days_in_this_month) {
-        daysTemplate.push(<tr> {rowData}</tr>);
-        rowData = [];
-      }
+//       rowData.push(
+//         <td
+//           onClick={() => updateSelectedDate(day_counter)}
+//           style={{
+//             backgroundColor: "9999cc",
+//             color: "000000",
+//             textAlign: "center",
+//           }}
+//         >
+//           {day_counter}
+//         </td>
+//       );
+//       if (week_day === 0 || day_counter === days_in_this_month) {
+//         daysTemplate.push(<tr> {rowData}</tr>);
+//         rowData = [];
+//       }
 
-      week_day++;
-    }
-    return <>{daysTemplate}</>;
-  };
+//       week_day++;
+//     }
+//     return <>{daysTemplate}</>;
+//   };
 
-  let calendar_html = (
-    <table><div className='table'>
-      <thead>
-        <tr><div className='header'>
+//   const [show,setShow]=useState(true);
 
-          <td>
-            <button onClick={() => changeMonth("prev")}>Prev</button>
-          </td>
-          <td
-            colSpan="7"
-            style={{
-              backgroundColor: "9999cc",
-              color: "000000",
-              textAlign: "center",
-            }}
-          >
-            {months[month]} {year}{" "}
-          </td>
-          <td>
-            <button onClick={() => changeMonth("next")}>Next</button>
-          </td>
-          </div>
-        </tr>
-        <tr><div className="week">
-          {weekDays.map((d) => (
-            <td>{d}</td>
-          ))}
-        </div>
-        </tr>
-      </thead>
-      <tbody><div className='dates'>{getDays()}</div></tbody>
-    </div>
-    </table>
-  );
+//   return (
+    
+//     <div>
+//     <table><div className='table'>
+//       <thead>
+//         <tr><div className='header'>
 
-  return calendar_html;
+//           <td>
+//             <button onClick={() => changeMonth("prev")}>Prev</button>
+//           </td>
+//           <td
+//             colSpan="7"
+//             style={{
+//               backgroundColor: "9999cc",
+//               color: "000000",
+//               textAlign: "center",
+//             }}
+//           >
+//             {months[month]} {year}{" "}
+//           </td>
+//           <td>
+//             <button onClick={() => changeMonth("next")}>Next</button>
+//           </td>
+//           </div>
+//         </tr>
+//         <tr><div className="week">
+//           {weekDays.map((d) => (
+//             <td>{d}</td>
+//           ))}
+//         </div>
+//         </tr>
+//       </thead>
+//       <tbody><div className='dates'>{getDays()}</div></tbody>
+//     </div>
+//     </table>
+//     </div>
+//   );
+
+//   return calendar_html
+  
+// }
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Calendar from "./components/calendar";
+
+function App() {
+  return <Calendar />;
 }
+
+export default App;
